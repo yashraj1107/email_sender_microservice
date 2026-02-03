fac02a172b3eaec2cceca5f8bf1857eb37425fe5
yashraj1107 <yashrajsawant1107@gmail.com>
2026-02-03 20:26:34 +0530

    fixes

--- utils.py
+++ utils.py
@@ -15,6 +15,7 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
+
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
